18f346924356e3e8d60471633b869c41ebbfa6d2
Andreas Langenhagen <andreas.langenhagen@tier.app>
2021-09-23 01:10:05 +0200

    Put linter config into pyproject.toml & .flake8
    Frick what? Flake8 still doesn't support pyproject.toml? Doesn't even
    seem that they will? wtf?

--- scripts/lint-project.sh
+++ scripts/lint-project.sh
@@ -4,39 +4,13 @@
 # Run the script with an active, viable virtualenv from the project's root directory.
 # In order for the script to work correctly, run it from the project's root directory.
 
-root_path='src'
+root_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../src"
 
 printf -- '\e[1mFlake8...\e[0m\n'
-flake8_error_codes_array=(
-    'E128'  # continuation line under-indented for visual indent
-    'E501'  # line too long (87 > 79 characters)
-    'E712'  # comparison to False should be 'if cond is False:' or 'if not cond:'
-    'W503'  # line break after binary operator
-)
-flake8_error_codes="$(printf '%s,' "${flake8_error_codes_array[@]}")"
-find "$root_path" -iname "*.py" -exec \
-    flake8 --ignore="$flake8_error_codes" '{}' \;
+flake8 "$root_path";
 
 printf -- '\e[1mPylint...\e[0m\n'
-pylint_error_codes_array=(
-    'C0103'  # Does not conform to snake case style
-    'C0121'  # Comparison to False should be 'not expr' or 'expr is False'
-    'C0330'  # Wrong continued indentation (add 2 spaces).
-    'C0412'  # Imports from package matplotlib are not grouped
-    'E0401'  # Unable to import foo.bar
-    'R0903'  # Too few public methods
-    'R0913'  # Too many arguments
-    'R0914'  # Too many local variables
-    'W0603'  # Using the global statement
-    'W1203'  # Use lazy % formatting in logging functions
-
-)
-pylint_error_codes="$(printf '%s,' "${pylint_error_codes_array[@]}")"
-find "$root_path" -iname "*.py" -exec \
-    pylint \
-        --disable="$pylint_error_codes" \
-        --msg-template='{msg_id} {path}:{line}: {msg}' '{}' \
-        --score no \;
+pylint "$root_path" --msg-template='{msg_id} {path}:{line}: {msg}' --score no;
 
 printf -- '\e[1mShellcheck...\e[0m\n'
 shellcheck_exclude_codes_array=(
