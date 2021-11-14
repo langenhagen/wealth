64b0fdc17b1869843e2de11384b310e3dabb3418
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-11-14 11:15:17 +0100

    Consider single shares in Investment sets

--- src/wealth/invest.py
+++ src/wealth/invest.py
@@ -20,33 +20,52 @@ class TransactionType(Enum):
     buy = "buy"
     sell = "sell"
     dividend = "dividend"
+    capital_raise = "capital raise"
 
     def __str__(self):
         """Print the enum's value nicely."""
         return self.value
 
+    @staticmethod
+    def profit_types() -> tuple[
+        "TransactionType", "TransactionType", "TransactionType"
+    ]:
+        """Return all transaction types that denote incomes."""
+        return (
+            TransactionType.capital_raise,
+            TransactionType.dividend,
+            TransactionType.sell,
+        )
+
 
 class Transaction:
     """A singular transaction."""
 
-    def __init__(self, date: dt.date, type_: TransactionType, amount: float):
+    def __init__(
+        self, date: dt.date, type_: TransactionType, amount: float, shares: int
+    ):
         """Initialize the Transaction with the given date stamp as a string of
         type yyy-mm-dd and a transaction amount."""
-        self.date = date
         self.type_ = type_
+        self.date = date
+        assert amount > 0, "Amount must be > 0"
         self.amount = amount
+        assert shares >= 0, "Shares must be >= 0"
+        self.shares = shares
 
     @staticmethod
     def from_dict(data: dict[str, dict]) -> "Transaction":
         """Create a transaction from a dictionary-representation"""
         keys_ = list(data.keys())
-        assert len(keys_) == 1, "Found more than 1 key in a transaction argument!"
-        type_str = keys_[0]
-        transaction = data[type_str]
-        amount = transaction["amount"]
-        date = dateutil.parser.parse(str(transaction["date"]))
-        type_ = TransactionType(type_str)
-        return Transaction(date=date, type_=type_, amount=amount)
+        assert len(keys_) == 1, "Found more than 1 key in a transaction argument"
+        type_ = keys_[0]
+        transaction = data[type_]
+        return Transaction(
+            date=dateutil.parser.parse(str(transaction["date"])).date(),
+            type_=TransactionType(type_),
+            amount=transaction["amount"],
+            shares=transaction.get("shares", 0),
+        )
 
 
 @dataclass
@@ -55,7 +74,6 @@ class InvestmentSet:
 
     company: str
     transactions: list[Transaction]
-    is_open: bool = False  # whether or not positions are still invested
 
     def start_date(self) -> dt.date:
         """Returns the date of the first transaction."""
@@ -64,49 +82,88 @@ class InvestmentSet:
     def duration(self) -> int:
         """Denotes the duration of the holding in days, minimum 1 day."""
         min_ = min([t.date for t in self.transactions])
-        if self.is_open is not True:
-            max_ = max([t.date for t in self.transactions])
+        if self.is_open() is True:
+            max_ = dt.date.today()
         else:
-            max_ = dt.datetime.today()
+            max_ = max([t.date for t in self.transactions])
         delta = max_ - min_
         return delta.days + 1
+
+    def is_open(self) -> bool:
+        """Indicates whether all shares belonging to the investment set have
+        been sold or not."""
+        share_balance = 0
+        for transaction in self.transactions:
+            if transaction.type_ == TransactionType.buy:
+                share_balance += transaction.shares
+            elif transaction.type_ == TransactionType.sell:
+                share_balance -= transaction.shares
+
+        assert share_balance >= 0, "Cannot sell more shares than bought"
+        return bool(share_balance)
 
     def invested_sum(self) -> float:
         """The absolute total amount of money invested."""
         buy = TransactionType.buy
-        return abs(sum([t.amount for t in self.transactions if t.type_ is buy]))
+        return sum([t.amount for t in self.transactions if t.type_ is buy])
 
     def gross_profit(self) -> float:
         """The amount of gross profit."""
-        dividend, sell = TransactionType.dividend, TransactionType.sell
-        amount = sum(
-            [t.amount for t in self.transactions if t.type_ in (sell, dividend)]
-        )
+        types = TransactionType.profit_types()
+        amount = sum([t.amount for t in self.transactions if t.type_ in types])
         return amount - self.invested_sum()
 
-    def net_profit(self, tax_rate: Optional[float] = None) -> float:
+    def _get_individual_shares_and_prices(self, type_: TransactionType) -> list[float]:
+        """Return a list of individual share prices for all transactions with
+        the given transaction type in the same order as the items are in the
+        member `transactions`."""
+        transactions = [t for t in self.transactions if t.type_ == type_]
+        prices = []
+        for transaction in transactions:
+            price = transaction.amount / transaction.shares
+            prices.extend([price] * transaction.shares)
+
+        return prices
+
+    def net_profit(self, tax_rate: Optional[float] = None) -> Optional[float]:
         """The amount of profit after capital gains taxes on profits. Use the
         tax rate from the configs if the given tax rate is None."""
-        dividend, sell = TransactionType.dividend, TransactionType.sell
+        if self.is_open() is True:
+            return None
         tax_rate = tax_rate or wealth.config["capital_gains_taxrate"]
-        amount = sum(
-            [t.amount for t in self.transactions if t.type_ in (sell, dividend)]
-        )
-        profit = amount - self.invested_sum()
-        if profit > 0:
-            return profit * (1 - tax_rate)
-        else:
-            return profit
 
-    def net_performance(self) -> float:
-        """Returns the net performance in percent."""
+        buys = self._get_individual_shares_and_prices(TransactionType.buy)
+        sells = self._get_individual_shares_and_prices(TransactionType.sell)
+        assert len(buys) == len(sells), "Number of bought and sold shares must be equal"
+
+        profit = 0.0
+        for buy, sell in zip(buys, sells):
+
+            amount = sell - buy
+            if amount > 0:
+                profit += amount * (1 - tax_rate)
+            else:
+                profit += amount
+
+        types = (TransactionType.capital_raise, TransactionType.dividend)
+        transactions = [t.amount for t in self.transactions if t.type_ in types]
+        profit += sum(transactions) * (1 - tax_rate)
+
+        return profit
+
+    def net_performance(self) -> Optional[float]:
+        """Returns the net performance in percent if the investment set is not
+        open anymore."""
         sum_ = self.invested_sum()
-        return ((sum_ + self.net_profit()) / sum_ - 1) * 100
+        if (net_profit := self.net_profit()) is None:
+            return None
+        return ((sum_ + net_profit) / sum_ - 1) * 100
 
-    def net_performance_per_day(self) -> float:
+    def net_performance_per_day(self) -> Optional[float]:
         """Returns the net performance in percent divided by the number of
         days."""
-        return self.net_performance() / self.duration()
+        performance = self.net_performance()
+        return None if performance is None else performance / self.duration()
 
 
 def _load_stocks_yml() -> list[InvestmentSet]:
@@ -121,14 +178,10 @@ def _load_stocks_yml() -> list[InvestmentSet]:
         company = keys_[0]
         transactions = []
         details = set_[company]
-        is_open = False
         for detail in details:
-            if detail == "open":
-                is_open = True
-            else:
-                transactions.append(Transaction.from_dict(detail))
+            transactions.append(Transaction.from_dict(detail))
 
-        investments.append(InvestmentSet(company, transactions, is_open))
+        investments.append(InvestmentSet(company, transactions))
 
     return investments
 
@@ -137,7 +190,7 @@ def _summarize_closed_investments(investments: list[InvestmentSet]) -> pd.DataFr
     """Convert the given investment sets to a df that contains a summary of each
     closed investment set."""
 
-    past_investments = [i for i in investments if not i.is_open]
+    past_investments = [i for i in investments if not i.is_open()]
     past_df = pd.DataFrame(
         {
             "company": [i.company for i in past_investments],
@@ -168,7 +221,7 @@ def _summarize_open_investments(investments: list[InvestmentSet]) -> pd.DataFram
     """Convert the given investment sets to a df that contains a summary of each
     open investment set."""
 
-    past_investments = [i for i in investments if i.is_open]
+    past_investments = [i for i in investments if i.is_open()]
     past_df = pd.DataFrame(
         {
             "company": [i.company for i in past_investments],
@@ -191,11 +244,8 @@ def stock():
     n_investments = len(investments)
     print(f"I made {n_investments} investments.")
 
-    net_profits = sum([i.net_profit() for i in investments if i.is_open is False])
+    net_profits = sum([i.net_profit() for i in investments if i.is_open() is False])
     print(f"The net profits are {Money(net_profits)}.")
-
-    net_perf = sum([i.net_performance() for i in investments if i.is_open is False])
-    print(f"The net performance is {wealth.percent_fmt(net_perf)}")
 
     print("\nClosed investments:")
     _summarize_closed_investments(investments)
