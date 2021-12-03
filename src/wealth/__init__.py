17c52a940b92dadf8391436ca66ebf7e17d8295d
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-12-03 23:28:09 +0100

    Remove import about labels
    Superfluous and outdated.

--- src/wealth/__init__.py
+++ src/wealth/__init__.py
@@ -11,6 +11,5 @@ import wealth.track
 import wealth.transactions
 from wealth.config import config
 from wealth.importers import init
-from wealth.labels import expense_labels, income_labels
 from wealth.util.transaction_type import TransactionType
 from wealth.util.util import Money, money_fmt, percent_fmt
