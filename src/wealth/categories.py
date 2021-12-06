de608ad4ff16d24177cfd8cdf1270cc048aa05c6
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-12-06 23:06:34 +0100

    Improve categories.py
    Improve the code and the docstring.

--- src/wealth/categories.py
+++ src/wealth/categories.py
@@ -7,7 +7,10 @@ from wealth.util.util import Money, display_side_by_side
 
 
 def categories(df: pd.DataFrame, categories: dict[str, str]):
-    """Examine all given df's rows that match given categories."""
+    """Examine all given df's rows that match given categories.
+    Display a dataframe per each given category with monthly sums,
+    quarterly sums and averages and yearly sums and averages.
+    Extrapolate average values to full quarters or years."""
     dfs = []
     for regex in categories.values():
         rows = df[df["all_data"].str.contains(regex)]
@@ -16,13 +19,13 @@ def categories(df: pd.DataFrame, categories: dict[str, str]):
         monthly = rows["amount"].resample("M").sum()
         amount["monthly"] = monthly
 
-        quarterly = rows["amount"].resample("Q").sum()
-        amount["quarterly"] = quarterly
-        amount["quarterly avg"] = quarterly / 3
+        quarterly_resampler = monthly.resample("Q")
+        amount["quarterly"] = quarterly_resampler.sum()
+        amount["quarterly avg"] = quarterly_resampler.mean()
 
-        yearly = rows["amount"].resample("A").sum()
-        amount["yearly"] = yearly
-        amount["yearly avg"] = yearly / 12
+        yearly_resampler = monthly.resample("A")
+        amount["yearly"] = yearly_resampler.sum()
+        amount["yearly avg"] = yearly_resampler.mean()
 
         category_df = pd.DataFrame(amount)
         category_df = category_df.applymap(lambda x: "" if math.isnan(x) else Money(x))
