388bd9d22f4f0dd3f226f8adfa9436fde0a47617
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-01-07 18:23:11 +0100

    Make `config.py` more readable

--- src/wealth/config.py
+++ src/wealth/config.py
@@ -1,36 +1,48 @@
 """Contains code to read the yaml-configuration."""
 import logging
+from copy import deepcopy
 from typing import Any, Dict
 
-import dateutil.parser
+import dateutil
 import yaml
+
+default = {
+    "capital_gains_taxrate": 0.27,
+    "currency": "€",
+    "inflation_rate": 2,
+    "retirement": {
+        "birthday": "2000-01-01",
+        "retirement_age": 67,
+    },
+}
 
 
 def _read_config_yaml() -> Dict[str, Any]:
-    """Read the file `config.yml` and write its contents to a dict.
-    Give defaults to values that the file doesn't specify."""
+    """Read the file `config.yml` and write its contents to a dict."""
     filename = "config.yml"
     try:
         with open(f"../config/{filename}", encoding="UTF-8") as file:
-            config_ = yaml.load(file, Loader=yaml.FullLoader)
+            return yaml.safe_load(file)
     except FileNotFoundError:
         logging.getLogger().warning(f"Could not open {filename}.")
         return {}
 
-    config_["capital_gains_taxrate"] = config_.get("capital_gains_taxrate", 0.27)
-    config_["currency"] = config_.get("currency", "€")
-    config_["inflation_rate"] = config_.get("inflation_rate", 2)
 
-    config_["retirement"] = retirement = config_.get("retirement", {})
-    birthday_str = retirement.get("birthday", "2000-01-01")
-    birthday = dateutil.parser.parse(birthday_str)
-    retirement["birthday"] = birthday
+def _create_config() -> Dict[str, Any]:
+    """Read the file `config.yml` from file and give defaults to values that the
+    file doesn't specify."""
+    user = _read_config_yaml()
+    config_ = deepcopy(default)
+    config_.update(user)
 
-    retirement["retirement_age"] = retirement.get("retirement_age", 67)
-    retirement_age = retirement["retirement_age"]
-    retirement["retirement_year"] = retirement["birthday"].year + retirement_age
+    retire = deepcopy(default["retirement"])
+    retire.update(user.get("retirement", {}))
+
+    retire["birthday"] = dateutil.parser.parse(retire["birthday"])
+    retire["retirement_year"] = retire["birthday"].year + retire["retirement_age"]
+    config_["retirement"] = retire
 
     return config_
 
 
-config = _read_config_yaml()
+config = _create_config()
