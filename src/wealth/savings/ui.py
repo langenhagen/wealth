5467826ba88d8e16021c3c779bf0ee81c4efba86
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-01-14 00:19:31 +0100

    Plot account develoment in savings

--- src/wealth/savings/ui.py
+++ src/wealth/savings/ui.py
@@ -1,10 +1,34 @@
 """User Interface related code for savings."""
+import matplotlib.pyplot as plt
 import pandas as pd
 from ipywidgets import Output, VBox
 
 from wealth.ui.display import display
 from wealth.ui.format import date_fmt, money_fmt, ratio_fmt
+from wealth.ui.plot import setup_yearly_plot_and_axes
 from wealth.ui.widgets import create_inflation_widgets, create_interest_widgets
+
+
+def _display_account_history_df(df: pd.DataFrame):
+    """Display the account history DataFrame."""
+    style = df.style.format(
+        formatter={
+            "date": date_fmt,
+            "amount": money_fmt(),
+            "net amount": money_fmt(),
+            "net amount after inflation": money_fmt(),
+            "balance": money_fmt(),
+            "net balance": money_fmt(),
+            "deposit cumsum": money_fmt(),
+            "deposit cumsum after inflation": money_fmt(),
+            "interest cumsum": money_fmt(),
+            "net interest cumsum": money_fmt(),
+            "net balance after inflation": money_fmt(),
+        },
+        na_rep="",
+    )
+
+    display(style)
 
 
 def _display_summary(df: pd.DataFrame):
@@ -43,25 +67,24 @@ def _display_summary(df: pd.DataFrame):
     display(style)
 
 
-def _display_account_history_df(df: pd.DataFrame):
-    """Display the account history DataFrame."""
-    style = df.style.format(
-        formatter={
-            "date": date_fmt,
-            "amount": money_fmt(),
-            "net amount": money_fmt(),
-            "net amount after inflation": money_fmt(),
-            "balance": money_fmt(),
-            "net balance": money_fmt(),
-            "deposit cumsum": money_fmt(),
-            "interest cumsum": money_fmt(),
-            "net interest cumsum": money_fmt(),
-            "net balance after inflation": money_fmt(),
-        },
-        na_rep="",
-    )
+def _plot_account_history(df: pd.DataFrame):
+    """Plot given account history dataframe to display its development."""
+    cols2colors = {
+        "balance": "#0000ff",
+        "net balance": "#8888ff",
+        "net balance after inflation": "#ff0000",
+        "deposit cumsum": "#008800",
+        "deposit cumsum after inflation": "#00ff00",
+    }
+    cols = list(cols2colors.keys())
+    colors = list(cols2colors.values())
 
-    display(style)
+    df = df[["date"] + cols].groupby(["date"]).last()
+
+    for col, color in cols2colors.items():
+        plt.plot(df.index, df[col], drawstyle="steps-post", color=color, label=col)
+
+    plt.legend(loc="best", borderaxespad=0.1)
 
 
 def render(
@@ -80,6 +103,13 @@ def render(
     with out_summary:
         _display_summary(summary)
 
+    out_fig = Output()
+    with out_fig:
+        fig = plt.figure(figsize=(10, 7), num="Account Development")
+        fig.clear()
+        setup_yearly_plot_and_axes(fig, "Account Development")
+        _plot_account_history(df)
+
     out_df = Output()
     with out_df:
         _display_account_history_df(df)
@@ -89,8 +119,8 @@ def render(
     display(out_summary)
 
     display("# Account Development")
+    display(out_fig)
     display(out_df)
 
     # TODO write observer
     # TODO wire widgets
-    # TODO graph, maybe in sep file
