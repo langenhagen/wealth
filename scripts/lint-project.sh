e24ee98a98cc3e7f8251b046b7ad44ef38b05764
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-01-22 00:44:03 +0100

    Adjust and improve the linter and its configs

--- scripts/lint-project.sh
+++ scripts/lint-project.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Run linters with the project's preferences.
-# Make sure that the linters flake8, pylint, mypy and shellcheck are available on your system.
+# Runs the tools Flake8, Pylint, Mypy, Black, Isort, Vulture and Shellcheck one after another.
 # Run the script with an active, viable virtualenv from the project's root directory.
-# In order for the script to work correctly, run it from the project's root directory.
 
 root_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../src"
 
@@ -10,16 +9,25 @@ printf -- '\e[1mFlake8...\e[0m\n'
 flake8 "$root_path"
 
 printf -- '\e[1mPylint...\e[0m\n'
-pylint "$root_path" --msg-template='{msg_id} {path}:{line}: {msg}' --score no
+pylint "$root_path" --msg-template='{path}:{line}: {msg_id} {symbol} {msg}' --score no
 
 printf -- '\e[1mMypy...\e[0m\n'
-mypy --ignore-missing-imports --follow-imports=skip --no-color-output --no-error-summary "$root_path"
+mypy \
+    --follow-imports=skip \
+    --ignore-missing-imports \
+    --no-color-output \
+    --no-error-summary \
+    --show-error-codes \
+    "$root_path"
 
 printf -- '\e[1mBlack...\e[0m\n'
 black --check --quiet "$root_path" || printf 'The black formatting is bad\n'
 
 printf -- '\e[1mIsort...\e[0m\n'
 isort --profile black --check-only --diff "$root_path"
+
+printf -- '\e[1mVulture...\e[0m\n'
+vulture --min-confidence 100 "$root_path"
 
 printf -- '\e[1mShellcheck...\e[0m\n'
 shellcheck_exclude_codes_array=(
