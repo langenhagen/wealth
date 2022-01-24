1af8b25ac61d2453715c2fcc732ca68902b4b4d3
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-01-24 01:55:31 +0100

    Improve lint-project.sh

--- scripts/lint-project.sh
+++ scripts/lint-project.sh
@@ -3,31 +3,41 @@
 # Runs the tools Flake8, Pylint, Mypy, Black, Isort, Vulture and Shellcheck one after another.
 # Run the script with an active, viable virtualenv from the project's root directory.
 
-root_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../src"
+lint_python_files() {
+    # Lint the given python file or lont all Python files under the given folder.
+    path="$1"
+    printf -- '\e[1mFlake8...\e[0m\n'
+    flake8 "$path"
 
-printf -- '\e[1mFlake8...\e[0m\n'
-flake8 "$root_path"
+    printf -- '\e[1mPylint...\e[0m\n'
+    pylint "$path" --msg-template='{path}:{line}: {msg_id} {symbol} {msg}' --score no
 
-printf -- '\e[1mPylint...\e[0m\n'
-pylint "$root_path" --msg-template='{path}:{line}: {msg_id} {symbol} {msg}' --score no
+    printf -- '\e[1mMypy...\e[0m\n'
+    mypy \
+        --follow-imports=skip \
+        --ignore-missing-imports \
+        --no-color-output \
+        --no-error-summary \
+        --show-error-codes \
+        "$path"
 
-printf -- '\e[1mMypy...\e[0m\n'
-mypy \
-    --follow-imports=skip \
-    --ignore-missing-imports \
-    --no-color-output \
-    --no-error-summary \
-    --show-error-codes \
-    "$root_path"
+    printf -- '\e[1mBlack...\e[0m\n'
+    black --check --quiet "$path" || printf 'The black formatting is bad\n'
 
-printf -- '\e[1mBlack...\e[0m\n'
-black --check --quiet "$root_path" || printf 'The black formatting is bad\n'
+    printf -- '\e[1mIsort...\e[0m\n'
+    isort --profile black --check-only --diff "$path"
 
-printf -- '\e[1mIsort...\e[0m\n'
-isort --profile black --check-only --diff "$root_path"
+    printf -- '\e[1mVulture...\e[0m\n'
+    vulture --min-confidence 100 "$path"
+}
 
-printf -- '\e[1mVulture...\e[0m\n'
-vulture --min-confidence 100 "$root_path"
+root_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../src/"
+
+printf '\e[1msetup.py...\e[0m\n'
+lint_python_files "$root_path/setup.py"
+
+printf '\e[1mOther Python code...\e[0m\n'
+lint_python_files "$root_path/wealth/"
 
 printf -- '\e[1mShellcheck...\e[0m\n'
 shellcheck_exclude_codes_array=(
